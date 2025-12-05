a1f83d1d44807d050270e38950b8d55a9d844e10
xXYxxdMC <xXYxxdMC@outlook.com>
2025-12-05 22:10:46 +0800

    Update

--- bin/json_source_test.dart
+++ bin/json_source_test.dart
@@ -6,43 +6,49 @@ import 'package:uuid/uuid.dart';
 
 import 'package:crypto/crypto.dart';
 
-var token = "";
+var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzY1OGI2NDhmODA5NWU5ZGM1ZWYyYzciLCJlbWFpbCI6Inh4eXh4ZG1jYmtiayIsInJvbGUiOiJtZW1iZXIiLCJuYW1lIjoieHh5eHhkbWNiayIsInZlcnNpb24iOiIyLjIuMS4zLjMuNCIsImJ1aWxkVmVyc2lvbiI6IjQ1IiwicGxhdGZvcm0iOiJhbmRyb2lkIiwiaWF0IjoxNzY0OTM4OTA3LCJleHAiOjE3NjU1NDM3MDd9.7MCMKMHAUC2kDC6U6mPVG8XUUh0GslZe6kJX-RnoHts";
 
 const uuid = Uuid();
 final uuidV1 = uuid.v1();
 final uuidV4 = uuid.v4();
+final time = DateTime.now().millisecondsSinceEpoch.toString();
+Map<String, String> runTimeValue = {};
 
 Future<void> main() async {
   var json = jsonDecode(await File("bin/pica.json").readAsString());
-  var header = decodeHeader(json);
-  var data = {
-    "email": "xxyxxdmcbkbk",
-    "password": "lgb20080216"
-  };
-  var dio = Dio();
-  //print(getHeaders("post", token, "auth/sign-in"));
-  //print(header);
-  dio.options = BaseOptions(headers: header..addAll({"content-length": data.toString().length.toString()}), responseType: ResponseType.plain, receiveDataWhenStatusError: true,);
-  var res = await dio.post("https://picaapi.picacomic.com/auth/sign-in",
-    options: Options(
-      responseType: ResponseType.plain,
-      validateStatus: (i) {
-        return i == 200 || i == 400 || i == 401;
-      },
-    ),
-    data: data
+  final action = "profile";
+  final uri = json["${action}_uri"].toString().startsWith("/") ? json["api_uri"].toString() + json["${action}_uri"].toString() : json["${action}_uri"].toString();
+  final method = json["${action}_method"].toString();
+  runTimeValue["uri"] = uri;
+  runTimeValue["method"] = method;
+  var data = {};
+  if (json["${action}_data"] != null) {
+    data = decodeHeaderOrData(json, isHeader: false, bodyKey: "${action}_data");
+  }
+  var dio = Dio()..options = BaseOptions(
+      headers: decodeHeaderOrData(json),
+      responseType: ResponseType.plain
   );
-  print(res.data);
+  var res = switch(method) {
+    "get" => await dio.get(uri,
+        data: data
+    ),
+    "post" => await dio.post(uri,
+        data: data
+    ),
+    _ => {}
+  };
+  if (res is Response<dynamic>) print(res.data);
 }
 
-Map<String, String> decodeHeader(Map<String, dynamic> json) {
-  var rawHeader = Map<String, dynamic>.from(json['header']);
+Map<String, String> decodeHeaderOrData(Map<String, dynamic> json, {bool isHeader = true, String bodyKey = ""}) {
+  var rawHeaderOrData = Map<String, dynamic>.from(json[isHeader ? 'header' : bodyKey]);
   var extra = json['extra'] ?? {};
   var settings = json['settings'] ?? {};
 
   Map<String, String> resolved = {};
 
-  rawHeader.forEach((key, value) {
+  rawHeaderOrData.forEach((key, value) {
     if (!key.startsWith("^")) resolved[key] = resolveValue(value, json, extra, settings);
   });
 
@@ -51,8 +57,10 @@ Map<String, String> decodeHeader(Map<String, dynamic> json) {
 
 String resolveValue(dynamic value, Map<String, dynamic> json,
     Map<String, dynamic> extra, Map<String, dynamic> settings, {
-      String? hintKey,
+      String? hintKey
     }) {
+
+
   if (value == null) return "";
 
   if (value is Map) {
@@ -126,7 +134,7 @@ String getConstants(String key) {
   return switch (key) {
     "uuid1" => uuidV1,
     "uuid4" => uuidV4,
-    "time" => DateTime.now().millisecondsSinceEpoch.toString(),
+    "time" => time,
     _ => ""
   };
 }
@@ -134,8 +142,10 @@ String getConstants(String key) {
 String getRuntimeValue(String key) {
   return switch (key) {
     "token" => token,
-    "uri" => "auth/sign-in",
-    "method" => "POST",
+    "uri" => runTimeValue["uri"] ?? "",
+    "method" => runTimeValue["method"] ?? "",
+    "user_name" => "xxyxxdmcbkbk",
+    "password" => "lgb20080216",
     _ => ""
   };
 }
@@ -144,26 +154,34 @@ dynamic executeMethod(Map<String, dynamic> method,
     Map<String, dynamic> json, Map<String, dynamic> extra, Map<String, dynamic> settings, {
 dynamic input = "",
 }) {
-  var way = method['way'];
-  if (way.toString().startsWith(r"&")) return getConstants(way.toString().substring(1));
-  switch (way) {
-    case "#stringMerge":
+  var way = method['way'] as String;
+  switch (way.substring(1)) {
+    case "input":
+      var value = method['value'].toString();
+      return resolveValue(value, json, extra, settings);
+    case "stringMerge":
       var values = method['values'] as List;
       return values
           .map((v) => resolveValue(v, json, extra, settings))
           .join("");
-    case "#lowerCase":
+    case "lowerCase":
       return input.toString().toLowerCase();
-    case "#utf8.encode":
+    case "utf8.encode":
       return utf8.encode(input.toString());
-    case "#HMAC-SHA256":
+    case "HMAC-SHA256":
       var secret = method["secret"].toString();
       var hmacSha256 = Hmac(sha256, utf8.encode(secret));
       return hmacSha256.convert(utf8.encode(input));
-    case "#replaceAll":
+    case "replaceAll":
       var keys = method['keys'] as List;
       return input.replaceAll(keys[0], keys[1]);
-    case "#~/":
+    case "+":
+      var key = method['key'] as int;
+      return (int.parse(input) ~/ key).toString();
+    case "-":
+      var key = method['key'] as int;
+      return (int.parse(input) ~/ key).toString();
+    case "~/":
       var key = method['key'] as int;
       return (int.parse(input) ~/ key).toString();
     default:
@@ -184,7 +202,6 @@ String resolveSettings(
   }
   return options.isNotEmpty ? options.first.toString() : "";
 }
-
 String executeEncryption(
     Map<String, dynamic> json,
     Map<String, dynamic> extra,
@@ -193,25 +210,25 @@ String executeEncryption(
   dynamic current;
 
   for (var step in steps) {
-    var way = step["way"];
-    switch (way) {
-      case "#stringMerge":
+    var way = step["way"] as String;
+    switch (way.substring(1)) {
+      case "stringMerge":
         var values = step["values"] as List;
         current = values
             .map((v) => resolveValue(v, json, extra, settings))
             .join();
         break;
 
-      case "#lowerCase":
+      case "lowerCase":
         current = current.toLowerCase();
         break;
 
-      case "#utf8.encode":
+      case "utf8.encode":
         current = utf8.encode(current);
         break;
 
-      case "#HMAC-SHA256":
-        var secret = utf8.encode(r'~d}$Q7$eIni=V)9\RK/P.RM4;9[7|@/CA}b~OW!3?EV`:<>M7pddUBL5n|0/*Cn');
+      case "HMAC-SHA256":
+        var secret = utf8.encode(step["secret"].toString());
         var hmacSha256 = Hmac(sha256, secret);
         if (current is List<int>) {
           current = hmacSha256.convert(current);
@@ -227,48 +244,4 @@ String executeEncryption(
   }
 
   return current.toString();
-}
-
-var apiKey = "C69BAF41DA5ABD1FFEDC6D2FEA56B";
-
-String createNonce() {
-  var uuid = const Uuid();
-  String nonce = uuid.v1();
-  return nonce.replaceAll("-", "");
-}
-
-String createSignature(String path, String nonce, String time, String method) {
-  String key = path + time + nonce + method + apiKey;
-  String data =
-      r'~d}$Q7$eIni=V)9\RK/P.RM4;9[7|@/CA}b~OW!3?EV`:<>M7pddUBL5n|0/*Cn';
-  var s = utf8.encode(key.toLowerCase());
-  var f = utf8.encode(data);
-  var hmacSha256 = Hmac(sha256, f);
-  var digest = hmacSha256.convert(s);
-  return digest.toString();
-}
-
-Map<String, String> getHeaders(String method, String token, String url) {
-  var nonce = createNonce();
-  var time = (DateTime.now().millisecondsSinceEpoch ~/ 1000).toString();
-  var signature = createSignature(url, nonce, time, method);
-  var headers = {
-    "api-key": "C69BAF41DA5ABD1FFEDC6D2FEA56B",
-    "accept": "application/vnd.picacomic.com.v1+json",
-    "app-channel": "3",
-    "authorization": token,
-    "time": (int.parse(getConstants("time")) ~/ 1000).toString(),
-    "nonce": nonce,
-    "app-version": "2.2.1.3.3.4",
-    "app-uuid": "defaultUuid",
-    "image-quality": "original",
-    "app-platform": "android",
-    "app-build-version": "45",
-    "Content-Type": "application/json; charset=UTF-8",
-    "user-agent": "okhttp/3.8.1",
-    "version": "v1.4.1",
-    "Host": "picaapi.picacomic.com",
-    "signature": signature,
-  };
-  return headers;
 }
